6a6e219988794c438ca536c3b638a8513cfed8da
starikovMark <supermark2005@mail.ru>
2023-11-18 07:52:52 +0700

    добавил: адаптив к блоку с маршрутами

--- js/script.js
+++ js/script.js
@@ -42,11 +42,11 @@ const swiperRoute = new Swiper(".swiper-route", {
     },
     // when window width is >= 480px
     788: {
-      slidesPerView: 2,
+      slidesPerView: 1,
     },
     // when window width is >= 640px
     992: {
-      slidesPerView: 3,
+      slidesPerView: 1,
     },
     1200: {
       slidesPerView: 1,
