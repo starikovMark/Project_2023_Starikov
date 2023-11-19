150215de8cba91e7b2aa949d991b4dc8a00af684
starikovMark <supermark2005@mail.ru>
2023-11-19 17:13:43 +0700

    изменил: фон мобильного меню

--- js/script.js
+++ js/script.js
@@ -2,22 +2,33 @@ const navbar = document.querySelector(".navbar");
 const mMenuToggle = document.querySelector(".navbar-menu-toggle");
 const menu = document.querySelector(".navbar-mobile");
 const modalDialog = document.querySelector(".modal.dialog");
+const header = document.querySelector(".header");
+const mobileItem = document.querySelectorAll(".navbar-menu-link");
+
 const openMenu = (event) => {
   // Стрелочная функция открывания меню
   menu.classList.add("is-open"); // Добавляем класс к меню
   mMenuToggle.classList.add("close-menu"); // Добавляем класс к кнопке меню
   document.body.style.overflow = "hidden"; // Когда меню открыто, страница не двигается
+  header.classList.add("is-open");
 };
 const closeMenu = (event) => {
   // Функция закрытия меню
   menu.classList.remove("is-open"); // Удаляем класс с меню
   mMenuToggle.classList.remove("close-menu"); // Удаляем класс с кнопки меню
   document.body.style.overflow = ""; // Страница двигается
+  header.classList.remove("is-open");
 };
 mMenuToggle.addEventListener("click", (event) => {
   // Функция при клике на кнопку меню
   event.preventDefault(); // Отключаем переход по ссылке для кнопки меню
   menu.classList.contains("is-open") ? closeMenu() : openMenu(); // если меню содержит класс is-open, выполняется функция закрытия меню, в противном случае меню открывается.
+});
+
+mobileItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    closeMenu();
+  });
 });
 
 const swiperRoute = new Swiper(".swiper-route", {
@@ -96,3 +107,5 @@ const swiperGallery = new Swiper(".swiper-gallery", {
     },
   },
 });
+
+console.log(mobileItem);
